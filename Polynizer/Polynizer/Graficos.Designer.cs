9cdebb54227800bab7306054b6471ea38c7a4600
Daniel <danidiaza@gmail.com>
2018-07-15 12:26:08 -0600

    Agregar pantalla de gráficos.

---
+++ Polynizer/Polynizer/Graficos.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Polynizer
+{
+    partial class Graficos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.comboBoxCharts = new System.Windows.Forms.ComboBox();
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.textBoxFilter = new System.Windows.Forms.TextBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.buttonRegresar = new System.Windows.Forms.Button();
+            this.buttonExportar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBoxCharts
+            // 
+            this.comboBoxCharts.FormattingEnabled = true;
+            this.comboBoxCharts.Location = new System.Drawing.Point(66, 79);
+            this.comboBoxCharts.Name = "comboBoxCharts";
+            this.comboBoxCharts.Size = new System.Drawing.Size(190, 24);
+            this.comboBoxCharts.TabIndex = 0;
+            this.comboBoxCharts.Text = "Seleccione el tipo...";
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.AutoSize = true;
+            this.labelTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitle.Location = new System.Drawing.Point(61, 22);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(129, 32);
+            this.labelTitle.TabIndex = 1;
+            this.labelTitle.Text = "Gráficos";
+            // 
+            // textBoxFilter
+            // 
+            this.textBoxFilter.Location = new System.Drawing.Point(459, 79);
+            this.textBoxFilter.Name = "textBoxFilter";
+            this.textBoxFilter.Size = new System.Drawing.Size(244, 22);
+            this.textBoxFilter.TabIndex = 2;
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(66, 119);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(637, 254);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "chart1";
+            // 
+            // buttonRegresar
+            // 
+            this.buttonRegresar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonRegresar.Location = new System.Drawing.Point(595, 394);
+            this.buttonRegresar.Name = "buttonRegresar";
+            this.buttonRegresar.Size = new System.Drawing.Size(108, 32);
+            this.buttonRegresar.TabIndex = 4;
+            this.buttonRegresar.Text = "Regresar";
+            this.buttonRegresar.UseVisualStyleBackColor = true;
+            // 
+            // buttonExportar
+            // 
+            this.buttonExportar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonExportar.Location = new System.Drawing.Point(459, 394);
+            this.buttonExportar.Name = "buttonExportar";
+            this.buttonExportar.Size = new System.Drawing.Size(108, 32);
+            this.buttonExportar.TabIndex = 5;
+            this.buttonExportar.Text = "Exportar";
+            this.buttonExportar.UseVisualStyleBackColor = true;
+            // 
+            // Graficos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 450);
+            this.Controls.Add(this.buttonExportar);
+            this.Controls.Add(this.buttonRegresar);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.textBoxFilter);
+            this.Controls.Add(this.labelTitle);
+            this.Controls.Add(this.comboBoxCharts);
+            this.Name = "Graficos";
+            this.Text = "Graficos";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxCharts;
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.TextBox textBoxFilter;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button buttonRegresar;
+        private System.Windows.Forms.Button buttonExportar;
+    }
+}
--- Polynizer/Polynizer/UsuarioPrincipal.Designer.cs
+++ Polynizer/Polynizer/UsuarioPrincipal.Designer.cs
@@ -113,7 +113,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(545, 389);
+            this.ClientSize = new System.Drawing.Size(545, 298);
             this.Controls.Add(this.labelTokens);
             this.Controls.Add(this.linkLabelCerrarSesion);
             this.Controls.Add(this.linkLabelPerfil);
