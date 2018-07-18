f162240e034e3371c7fa105900274d4b0c443fea
Ariel Arias <ariel.ariasrojas@ucr.ac.cr>
2018-07-17 19:53:46 -0600

    Eliminar caja de texto en graficos

--- Polynizer/Polynizer/Graficos.Designer.cs
+++ Polynizer/Polynizer/Graficos.Designer.cs
@@ -28,12 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea7 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend7 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.comboBoxCharts = new System.Windows.Forms.ComboBox();
             this.labelTitle = new System.Windows.Forms.Label();
-            this.textBoxFilter = new System.Windows.Forms.TextBox();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.buttonRegresar = new System.Windows.Forms.Button();
             this.buttonExportar = new System.Windows.Forms.Button();
@@ -46,10 +45,10 @@
             this.comboBoxCharts.FormattingEnabled = true;
             this.comboBoxCharts.Items.AddRange(new object[] {
             "Canciones procesadas por usuario"});
-            this.comboBoxCharts.Location = new System.Drawing.Point(67, 79);
-            this.comboBoxCharts.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.comboBoxCharts.Location = new System.Drawing.Point(50, 64);
+            this.comboBoxCharts.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.comboBoxCharts.Name = "comboBoxCharts";
-            this.comboBoxCharts.Size = new System.Drawing.Size(328, 24);
+            this.comboBoxCharts.Size = new System.Drawing.Size(247, 21);
             this.comboBoxCharts.TabIndex = 0;
             this.comboBoxCharts.SelectedIndexChanged += new System.EventHandler(this.comboBoxCharts_SelectedIndexChanged);
             // 
@@ -57,44 +56,37 @@
             // 
             this.labelTitle.AutoSize = true;
             this.labelTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelTitle.Location = new System.Drawing.Point(61, 22);
+            this.labelTitle.Location = new System.Drawing.Point(46, 18);
+            this.labelTitle.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelTitle.Name = "labelTitle";
-            this.labelTitle.Size = new System.Drawing.Size(129, 32);
+            this.labelTitle.Size = new System.Drawing.Size(101, 26);
             this.labelTitle.TabIndex = 1;
             this.labelTitle.Text = "Gráficos";
             // 
-            // textBoxFilter
-            // 
-            this.textBoxFilter.Location = new System.Drawing.Point(459, 79);
-            this.textBoxFilter.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.textBoxFilter.Name = "textBoxFilter";
-            this.textBoxFilter.Size = new System.Drawing.Size(244, 22);
-            this.textBoxFilter.TabIndex = 2;
-            // 
             // chart1
             // 
-            chartArea7.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea7);
-            legend7.Name = "Legend1";
-            this.chart1.Legends.Add(legend7);
-            this.chart1.Location = new System.Drawing.Point(67, 119);
-            this.chart1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(50, 97);
+            this.chart1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.chart1.Name = "chart1";
-            series7.ChartArea = "ChartArea1";
-            series7.Legend = "Legend1";
-            series7.Name = "Series1";
-            this.chart1.Series.Add(series7);
-            this.chart1.Size = new System.Drawing.Size(637, 254);
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(478, 206);
             this.chart1.TabIndex = 3;
             this.chart1.Text = "chart1";
             // 
             // buttonRegresar
             // 
             this.buttonRegresar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonRegresar.Location = new System.Drawing.Point(595, 394);
-            this.buttonRegresar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonRegresar.Location = new System.Drawing.Point(446, 320);
+            this.buttonRegresar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonRegresar.Name = "buttonRegresar";
-            this.buttonRegresar.Size = new System.Drawing.Size(108, 32);
+            this.buttonRegresar.Size = new System.Drawing.Size(81, 26);
             this.buttonRegresar.TabIndex = 4;
             this.buttonRegresar.Text = "Regresar";
             this.buttonRegresar.UseVisualStyleBackColor = true;
@@ -103,10 +95,10 @@
             // buttonExportar
             // 
             this.buttonExportar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonExportar.Location = new System.Drawing.Point(459, 394);
-            this.buttonExportar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonExportar.Location = new System.Drawing.Point(344, 320);
+            this.buttonExportar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonExportar.Name = "buttonExportar";
-            this.buttonExportar.Size = new System.Drawing.Size(108, 32);
+            this.buttonExportar.Size = new System.Drawing.Size(81, 26);
             this.buttonExportar.TabIndex = 5;
             this.buttonExportar.Text = "Exportar";
             this.buttonExportar.UseVisualStyleBackColor = true;
@@ -114,17 +106,16 @@
             // 
             // Graficos
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(781, 450);
+            this.ClientSize = new System.Drawing.Size(586, 366);
             this.Controls.Add(this.buttonExportar);
             this.Controls.Add(this.buttonRegresar);
             this.Controls.Add(this.chart1);
-            this.Controls.Add(this.textBoxFilter);
             this.Controls.Add(this.labelTitle);
             this.Controls.Add(this.comboBoxCharts);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.Name = "Graficos";
             this.Text = "Graficos";
@@ -139,7 +130,6 @@
 
         private System.Windows.Forms.ComboBox comboBoxCharts;
         private System.Windows.Forms.Label labelTitle;
-        private System.Windows.Forms.TextBox textBoxFilter;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
         private System.Windows.Forms.Button buttonRegresar;
         private System.Windows.Forms.Button buttonExportar;
